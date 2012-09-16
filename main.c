04c174b2771db0e01f6e0276ab4cd6d10509623c
trentonknight <trentonknight@gmail.com>
2012-09-16 18:16:15 -0400

    big revise...

--- main.c
+++ main.c
@@ -13,35 +13,60 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-void createFile();
+
 void setRights(int oct);
-void writeToFile();
+void writeToFile(char * flink);
+void appendToFile(char * flink);
+void referenceFile(char * flink);
 
 int main(int argc, const char * argv[])
 {
   int oct = 0600;
+  char *fileLink = "hwk3-4.txt";
 
-    createFile();
     setRights(oct);
-    writeToFile();
+    writeToFile(fileLink);
     oct = 0660;
     setRights(oct); 
+    appendToFile(fileLink);
+    referenceFile(fileLink);
     return 0;
 }
 
-void createFile(){
-    
-    system("touch hwk3-4.txt");
-    
-}
+
 void setRights(int oct){
     int flag;
     flag = chmod ("hwk3-4.txt", oct); /* Use an octal encoding */
     if (flag == -1) perror ("hwk3-4.txt");
 }
-void writeToFile(){
+void writeToFile(char * flink){
     int file;
-    file = open("hwk3-4.txt", O_WRONLY); /* Open file for writing */
-    write(file,"POSIX rocks!\n", 20);
+    file = open(flink, O_WRONLY | O_CREAT); /* Open file for writing */
+    write(file,"All work and no play ", 21);
     close(file);
+}
+void appendToFile(char * flink){
+  int file;
+  file = open(flink,  O_WRONLY | O_APPEND); /* Open file for writing */
+  write(file, "Makes Johnny a dull boy\n", 23);
+  close(file);
+}
+void referenceFile(char * flink){
+  FILE * pf;
+  int c;
+  
+  pf = fopen(flink, "r");
+
+  fseek(pf, 0L, SEEK_END);
+  long sz = ftell(pf);
+  fseek(pf, 10, SEEK_SET);
+
+ while(sz != 0)
+{
+printf("%c",c);
+c=fgetc(pf);
+--sz;
+printf("%ld",sz);
+}
+fclose(pf);
 }
